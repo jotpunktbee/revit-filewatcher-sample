0ce659815dbb0a6a7e1ec0865ec264ea82b6f8aa
Julian Bolliger <bolliger_julian@bluewin.ch>
2020-07-06 09:14:59 +0200

    Edit all informations

---
+++ FileWatcherSample/App.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace FileWatcherSample
+{
+    class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
