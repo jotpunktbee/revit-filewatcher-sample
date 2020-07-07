fba2d1a440eb0e0eb4bb5787392c4ad85d51b67e
Julian Bolliger <bolliger_julian@bluewin.ch>
2020-07-07 16:15:35 +0200

    Add OpenFolderDialog to DialogService

--- FileWatcherSample/Services/DialogService.cs
+++ FileWatcherSample/Services/DialogService.cs
@@ -1,23 +1,16 @@
-﻿using Autodesk.Revit.UI;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace FileWatcherSample.Services
 {
     public class DialogService : IDialogService
     {
-        #region Field
-        #endregion
-
-        #region Property
-        #endregion
-
-        #region Constructor
-        #endregion
-
         #region Method
         public void ShowErrorMessage(string message)
         {
@@ -26,12 +19,29 @@ namespace FileWatcherSample.Services
 
         public void ShowMessage(string title, string message)
         {
-            TaskDialog.Show(title, message);
+            Autodesk.Revit.UI.TaskDialog.Show(title, message);
         }
 
         public void ShowSucessMessage(string message)
         {
-            TaskDialog.Show("Success", message);
+            Autodesk.Revit.UI.TaskDialog.Show("Success", message);
+        }
+
+        public string OpenFolderDialog(string defaultDirectory, Window owner)
+        {
+            if (defaultDirectory == null)
+                defaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog()
+            {
+                InitialDirectory = defaultDirectory,
+                IsFolderPicker = true,
+            };
+
+            if (commonOpenFileDialog.ShowDialog(owner ?? null) == CommonFileDialogResult.Ok)
+                return commonOpenFileDialog.FileName;
+
+            return null;
         }
         #endregion
     }
--- FileWatcherSample/Services/IDialogService.cs
+++ FileWatcherSample/Services/IDialogService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FileWatcherSample.Services
 {
@@ -11,5 +12,6 @@ namespace FileWatcherSample.Services
         void ShowMessage(string title, string message);
         void ShowErrorMessage(string message);
         void ShowSucessMessage(string message);
+        string OpenFolderDialog(string defaultDirectory, Window owner);
     }
 }
